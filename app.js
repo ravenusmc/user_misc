9250f1726af47ab213266ad33e808831bf1c5f8f
ravenusmc <mcuddy77@gmail.com>
2017-08-27 15:56:26 -0400

    added ejs view engine

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+//Setting up the view engine 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.render('index');
 }); 
 
+//Code to start server
 app.listen(3000, function(){
   console.log('Server Started, check port 3000');
 });
