b2754a8a49dae776d6f7af849307d924abae4da5
ravenusmc <mcuddy77@gmail.com>
2017-08-28 20:10:24 -0400

    set up a public folder

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//Setting up public folder 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function(req, res){
   res.render('index');
 }); 
