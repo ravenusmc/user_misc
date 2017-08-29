fd60602c5c8f8ba85eaa45f62b358ab905dfc200
ravenusmc <mcuddy77@gmail.com>
2017-08-29 15:27:31 -0400

    added file for sign up page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/user_misc');
 let db = mongoose.connection;
@@ -11,12 +12,36 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//Body parser needed for working with forms!!!
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 //Setting up public folder 
 app.use(express.static(path.join(__dirname, 'public')));
 
+//This route will take the user to the home page.
 app.get('/', function(req, res){
   res.render('index');
 }); 
+
+//This route will log the user in. 
+app.post('/', function(req, res){
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  console.log(username);
+  console.log(password);
+
+});
+
+//This route take the user to the sign up page. 
+app.get('/sign_up', function(req, res){
+  res.render('sign_up');
+})
 
 //Code to start server
 app.listen(3000, function(){
